ba828c4102e83f02b40ac90b108ad5ddc8ad728e
jasonmarkbeaton <jason.beaton@gmail.com>
2015-10-28 15:15:28 -0400

    first commit

---
+++ leaderboard.js
@@ -0,0 +1,19 @@
+PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    'player': function(){
+        return PlayersList.find()
+    }
+  });
+
+  Template.leaderboard.events({
+    'click .player': function(){
+      console.log("you clicked a .player item");
+    }
+  });
+}
+
+if(Meteor.isServer){
+    // this code only runs on the server
+}
