ae0862294272066041e6e0384c676d55d64d621d
jasonmarkbeaton <jason.beaton@gmail.com>
2015-11-02 16:11:30 -0500

    Refactored player count

--- leaderboard.js
+++ leaderboard.js
@@ -29,15 +29,15 @@ if(Meteor.isClient){
     },
     'click .increment': function(){
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update(selectedPlayer, {$inc: {score: 5} });
+      Meteor.call('modifyPlayerScore', selectedPlayer, 5);
     },
     'click .decrement': function(){
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update(selectedPlayer, {$inc: {score: -5} });
+      Meteor.call('modifyPlayerScore', selectedPlayer, -5);
     },
     'click .remove': function(){
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.remove(selectedPlayer);
+      Meteor.call('removePlayerData', selectedPlayer);
     }
   });
 
@@ -46,9 +46,10 @@ if(Meteor.isClient){
       event.preventDefault();
       var playerNameVar = event.target.playerName.value;
       Meteor.call('insertPlayerData', playerNameVar);
-      }
-    });
-  }
+    }
+  });
+
+}
 
 if(Meteor.isServer){
 
@@ -57,14 +58,24 @@ if(Meteor.isServer){
     return PlayersList.find({createdBy: currentUserId})
   });
 
-  Meteor.methods({ 
+  Meteor.methods({
     'insertPlayerData': function(playerNameVar){
       var currentUserId = Meteor.userId();
       PlayersList.insert({
-        name: playerNameVar,
-        score: 0,
-        createdBy: currentUserId
-      })
+          name: playerNameVar,
+          score: 0,
+          createdBy: currentUserId
+      });
+    },
+    'removePlayerData': function(selectedPlayer){
+      var currentUserId = Meteor.userId();
+      PlayersList.remove({_id: selectedPlayer, createdBy: currentUserId});
+    },
+    'modifyPlayerScore': function(selectedPlayer, scoreValue){
+      var currentUserId = Meteor.userId();
+      PlayersList.update( {_id: selectedPlayer, createdBy: currentUserId},
+                          {$inc: {score: scoreValue} });
     }
   });
+
 }
